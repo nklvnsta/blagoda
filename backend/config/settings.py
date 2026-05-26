ae185decc38ceaf1a4397bf418fc257f0b37e2bd
Anastasia <nastanikol10105@gmail.com>
2026-05-27 02:37:02 +0300

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -74,8 +74,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
-    "https://blagoda-frontend-nklvnsta.amvera.io"
-    "https://blagoda-forecast.ru"
+    "https://blagoda-frontend-nklvnsta.amvera.io",
+    "https://blagoda-forecast.ru",
 
 ]
 CORS_ALLOWED_CREDENTIALS = True
